c0b99c93038a3f3d8d6f6870da273e30fc434b82
jeckyzileixu <jecky.zilei.xu@embracesource.com>
2015-12-24 11:22:28 +0800

    add nopassworf script

---
+++ nopassword_login/batch_set_nopassword_login.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ -z $* ];then
+   echo "Usage: $0 <ipfile> <username> <password>"
+   exit
+fi
+ipspath="$1"
+username="$2"
+userpwd="$3"
+successfile="./success"
+failedfile="./failed"
+rm -rf $successfile
+rm -rf $failedfile
+for ip in $(cat $ipspath)
+do
+    echo "<<<<<<<<<<<<<<<<<<<<<<<<<$ip<<<<<<<<<<<<<<<<<<<<"
+    ./run.exp $username $userpwd $ip
+    if [ $? -eq 0 ];then
+           echo $ip >> $successfile
+    else
+           echo $ip >> $failedfile
+    fi
+done
